0da9cf4474a79f95c70fbe83ddfe36e3c73850c3
Mhatw <contactomhatw@gmail.com>
2022-07-23 21:20:46 -0500

    FEAT: tableComponent second version

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,33 +2,63 @@ import {
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
-  Td,
   TableContainer,
+  Avatar,
+  Skeleton,
 } from "@chakra-ui/react";
 import React from "react";
 
-export function TableData() {
+export function TableData({ data }) {
   return (
-    <TableContainer>
+    <TableContainer bg={"white"} borderRadius="0.2rem" minH={"309.5px"}>
       <Table size="sm" variant="striped">
         <Thead>
-          <Tr>
-            <Th>Title</Th>
-            <Th>Title</Th>
-            <Th>Title</Th>
+          <Tr bg={"blue.500"}>
+            <Th color="whiteAlpha.900">Nombre</Th>
+            <Th color="whiteAlpha.900">Apellido</Th>
+            <Th color="whiteAlpha.900">Edad</Th>
+            <Th color="whiteAlpha.900">Genero</Th>
+            <Th color="whiteAlpha.900">Email</Th>
+            <Th color="whiteAlpha.900">Nacionalidad</Th>
+            <Th color="whiteAlpha.900">Foto</Th>
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>data</Td>
-            <Td>data</Td>
-            <Td>data</Td>
-          </Tr>
+          {data.map((item) => (
+            <Tr key={item.id}>
+              <Field>{item.name.first}</Field>
+              <Field>{item.name.last}</Field>
+              <Field>{item.dob.age}</Field>
+              <Field>{item.gender}</Field>
+              <Field>{item.email}</Field>
+              <Field>{item.nat}</Field>
+              <Field isAvatar>
+                <Avatar
+                  name={`${item.name.first} ${item.name.last}`}
+                  src={item.picture.thumbnail}
+                />
+              </Field>
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </TableContainer>
+  );
+}
+
+function Field({ children, isAvatar }) {
+  return (
+    <Th fontWeight="400">
+      <Skeleton
+        borderRadius={isAvatar ? "50%" : "0.2rem"}
+        isLoaded={true}
+        startColor="gray.100"
+        endColor="gray.300"
+      >
+        {children}
+      </Skeleton>
+    </Th>
   );
 }
