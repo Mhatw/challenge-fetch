ee2a80af34f069244b3ad8e0b79f9c841c3bf68d
Mhatw <contactomhatw@gmail.com>
2022-07-23 22:43:37 -0500

    FEAT add pagination butons and functions

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,21 +6,44 @@ import { useData } from "./context/DataContext";
 export default function Home() {
   const dataContext = useData();
 
+  function handlePage(number) {
+    dataContext.setCurrentPage(dataContext.currentPage + number);
+  }
+
   return (
     <>
-      {dataContext.loading && (
+      {dataContext.loadingView && (
         <Center w={"100vw"} h={"100vh"} bg={"gray.100"}>
           <Spinner thickness="4px" color="blue.500" size="xl" />
         </Center>
       )}
-      {!dataContext.loading && (
+      {!dataContext.loadingView && (
         <Center w={"100vw"} h={"100vh"} bg={"gray.100"} flexDirection="column">
           <Box>
-            <TableData
-              data={dataContext.data}
-              isLoading={dataContext.loading}
-            />
+            <TableData data={dataContext.data} />
           </Box>
+          <Center>
+            <Button
+              onClick={() => handlePage(-1)}
+              isDisabled={dataContext.currentPage === 1 || dataContext.loading}
+            >
+              Anterior
+            </Button>
+            <Box
+              borderRadius={"0.2rem"}
+              display={"inline"}
+              px="1rem"
+              bg={"whiteAlpha.900"}
+            >
+              {dataContext.currentPage}
+            </Box>
+            <Button
+              onClick={() => handlePage(1)}
+              isDisabled={dataContext.loading}
+            >
+              Siguiente
+            </Button>
+          </Center>
         </Center>
       )}
     </>
