dabbb0a93cd2e6bf0ce2630aa24407fe04997b3e
Mhatw <contactomhatw@gmail.com>
2022-07-23 21:21:48 -0500

    FEAT add DataProvider for homeView

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import { DataProvider } from "./context/DataContext";
 import Home from "./Home";
 
 function App() {
-  return <Home />;
+  return (
+    <DataProvider>
+      <Home />
+    </DataProvider>
+  );
 }
 
 export default App;
