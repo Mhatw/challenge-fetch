4060c7a37a30b6f4d38e6bae497b55727d1d49a0
Mhatw <contactomhatw@gmail.com>
2022-07-23 22:42:18 -0500

    FEAT add loadingView and useEffect for pagination

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -4,31 +4,45 @@ import { getData } from "../services/data-service";
 const DataContext = createContext(null);
 export const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [loadingView, setLoadingView] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  async function handleData() {
+  async function handleData(firstRequest, page) {
+    if (firstRequest) setLoadingView(true);
     setLoading(true);
+    console.log("handleData");
     try {
-      const response = await getData();
+      const response = await getData(page);
       setData(response.results);
       console.log(response.results);
     } catch (error) {
       console.log(error);
     }
     setLoading(false);
+    console.log("handleData end");
+    if (firstRequest) setLoadingView(false);
   }
 
   useEffect(() => {
-    handleData();
+    handleData(true);
   }, []);
+  useEffect(() => {
+    handleData(false, currentPage);
+  }, [currentPage]);
 
   return (
     <DataContext.Provider
       value={{
         data,
-        loading,
         setData,
+        loading,
         setLoading,
+        currentPage,
+        setCurrentPage,
+        loadingView,
+        setLoadingView,
+        handleData,
       }}
     >
       {children}
