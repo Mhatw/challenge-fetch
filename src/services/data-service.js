b976402bea4f9cba6faa6c68d3616695debced00
Mhatw <contactomhatw@gmail.com>
2022-07-23 21:22:53 -0500

    REFACTOR fetch for 5 results, only necessary data

--- src/services/data-service.js
+++ src/services/data-service.js
@@ -3,7 +3,9 @@ import { BASE_URL } from "./config";
 export async function getData(page, fields) {
   const options = { method: "GET" };
   const response = await fetch(
-    `${BASE_URL}?page=${page}&results=${fields}`,
+    `${BASE_URL}?page=${page || 1}&results=5&${
+      fields || "inc=name,dob,gender,email,nat,picture"
+    }`,
     options
   );
   const data = await response.json();
